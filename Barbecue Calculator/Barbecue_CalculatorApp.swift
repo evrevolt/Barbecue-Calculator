491cbdd732be85094033009b67f7bc890232ab3c
Intrantu <gennadi_wow@mail.ru>
2022-03-19 08:35:11 +0300

    Initial Commit

---
+++ Barbecue Calculator/Barbecue_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barbecue_CalculatorApp.swift
+//  Barbecue Calculator
+//
+//  Created by Геннадий Ведерников on 19.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Barbecue_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barbecue Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Barbecue Calculator
+//
+//  Created by Геннадий Ведерников on 19.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
